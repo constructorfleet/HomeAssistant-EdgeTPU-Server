76d1d79fb3f4dd231e12fbc13705284fd1948e79
Teagan glenn <teagan.m.glenn@gmail.com>
2019-11-18 01:40:32 -0700

    Fix setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='homeassistant_edgetpu_server',
+    name='edgetpu_server',
     version='1.0.0',
     license='MIT',
     url='https://github.com/constructorfleet/HomeAssistant-EdgeTPU-Server',
@@ -13,7 +13,6 @@ setuptools.setup(
     description='Performs object detection using an Edge Tensor Processing Unit on a video stream'
                 ' and publishes the state to a specified Home-Assistant instance.',
     long_description=long_description,
-    long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,

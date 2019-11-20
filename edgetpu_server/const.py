3c2cb8c241d46739960990033f28c3e45c14554f
Teagan glenn <teagan.m.glenn@gmail.com>
2019-11-20 02:32:35 -0700

    Const file

---
+++ edgetpu_server/const.py
@@ -0,0 +1,6 @@
+__version__ = '1.0.0'
+ENDPOINT_POST_STATE_TEMPLATE = "{}/api/states/{}"
+HEADER_AUTH_KEY = "Authorization"
+HEADER_AUTH_VALUE = "Bearer {}"
+HEADER_CONTENT_TYPE_KEY = "Content-Type"
+HEADER_CONTENT_TYPE_VALUE = "application/json"

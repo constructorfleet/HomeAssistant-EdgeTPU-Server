1c9046285bb71804fceab8e4928dc68573bc52f0
Teagan glenn <teagan.m.glenn@gmail.com>
2019-11-20 00:52:26 -0700

    Remove short args

--- edgetpu_server/__main__.py
+++ edgetpu_server/__main__.py
@@ -13,21 +13,21 @@ def main():
     )
     parser.add_argument('-f', '--config-file', is_config_file=True,
                         help="Configuration file path")
-    parser.add_argument("-m", "--models", required=True,
+    parser.add_argument("--models", required=True,
                         help="path to TensorFlow Lite object detection model")
-    parser.add_argument("-l", "--labels", required=True,
+    parser.add_argument("--labels", required=True,
                         help="path to labels file")
-    parser.add_argument("-c", "--confidence", type=float, default=70,
+    parser.add_argument("--confidence", type=float, default=70,
                         help="minimum probability to filter weak detections, percentage")
-    parser.add_argument("-s", "--stream", required=True,
+    parser.add_argument("--stream", required=True,
                         help="video stream to process")
-    parser.add_argument("-e", "--entity", requierd=True,
+    parser.add_argument("--entity", requierd=True,
                         help="Entity ID to push to Home-Assistant")
-    parser.add_argument("-t", "--categories", nargs="+", required=False,
+    parser.add_argument("--categories", nargs="+", required=False,
                         help="classifier types to report")
-    parser.add_argument("-u", "--haurl", required=True,
+    parser.add_argument("--haurl", required=True,
                         help="url for updating home-assistant states")
-    parser.add_argument("-a", "--token", required=True,
+    parser.add_argument("--token", required=True,
                         help="long lived home-assistant token for authentication")
     args = parser.parse_args()
 

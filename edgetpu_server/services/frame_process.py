2ae2f5f36e1f507c649a7a6fa39198467854226f
Teagan glenn <teagan.m.glenn@gmail.com>
2022-11-11 15:28:46 -0700

    Scale frame before processing

--- edgetpu_server/services/frame_process.py
+++ edgetpu_server/services/frame_process.py
@@ -4,6 +4,8 @@ from queue import Empty
 import cv2
 from multiprocessing.queues import Queue
 
+import imutils
+
 from edgetpu_server.services import Service
 from edgetpu_server.services.signals.frame_processed import FrameProcessedSignal
 from edgetpu_server.services.video_capture import VideoCaptureSignal
@@ -12,9 +14,16 @@ from edgetpu_server.services.video_capture import VideoCaptureSignal
 class FrameProcessService(Service):
     """Service for handling processing of frames."""
 
-    def __init__(self, name: str, capture_signal: VideoCaptureSignal, result_signal: FrameProcessedSignal):
+    def __init__(
+            self,
+            name: str,
+            scaled_width: int,
+            capture_signal: VideoCaptureSignal,
+            result_signal: FrameProcessedSignal,
+    ):
         """Initialize a new video capture service."""
         super().__init__(name)
+        self._scaled_width = scaled_width
         self._capture_signal = capture_signal
         self._result_signal = result_signal
 
@@ -24,7 +33,13 @@ class FrameProcessService(Service):
         while True:
             try:
                 self._capture_signal.request_latest_frame()
-                frame = self._capture_signal.retrieve_frame()
+                frame = cv2.cvtColor(  # pylint: disable=no-member
+                    imutils.resize(
+                        self._capture_signal.retrieve_frame(),
+                        width=self._scaled_width,
+                    ),
+                    cv2.COLOR_BGR2RGB  # pylint: disable=no-member
+                )  # pylint: disable=no-member
 
                 result = {}
                 self._result_signal.send_results(result)

88da2f9ca9236d1a5fb3702b8d192725a264e0f1
Teagan glenn <teagan.m.glenn@gmail.com>
2022-11-17 23:50:01 -0700

    FFMPEG

--- edgetpu_server/models/entity_stream.py
+++ edgetpu_server/models/entity_stream.py
@@ -11,4 +11,4 @@ class EntityStream:
         self.name = name
         self.entity_id = entity_id
         self.stream_url = stream_url
-        self.video_stream = cv2.VideoCapture(self.stream_url)  # pylint: disable=no-member
+        self.video_stream = cv2.VideoCapture(self.stream_url, cv2.CAP_FFMPEG)  # pylint: disable=no-member

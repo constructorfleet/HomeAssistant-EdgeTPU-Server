957ce899c9ecdf588f891f8ac8f8dc0221e73c62
Teagan glenn <teagan.m.glenn@gmail.com>
2022-11-18 00:15:16 -0700

    Stream props

--- edgetpu_server/models/entity_stream.py
+++ edgetpu_server/models/entity_stream.py
@@ -12,3 +12,16 @@ class EntityStream:
         self.entity_id = entity_id
         self.stream_url = stream_url
         self.video_stream = cv2.VideoCapture(self.stream_url, cv2.CAP_FFMPEG)  # pylint: disable=no-member
+
+        print("CV_CAP_PROP_FRAME_WIDTH: '{}'".format(self.video_stream.get(cv2.CAP_PROP_FRAME_WIDTH)))
+        print("CV_CAP_PROP_FRAME_HEIGHT : '{}'".format(self.video_stream.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+        print("CAP_PROP_FPS : '{}'".format(self.video_stream.get(cv2.CAP_PROP_FPS)))
+        print("CAP_PROP_POS_MSEC : '{}'".format(self.video_stream.get(cv2.CAP_PROP_POS_MSEC)))
+        print("CAP_PROP_FRAME_COUNT  : '{}'".format(self.video_stream.get(cv2.CAP_PROP_FRAME_COUNT)))
+        print("CAP_PROP_BRIGHTNESS : '{}'".format(self.video_stream.get(cv2.CAP_PROP_BRIGHTNESS)))
+        print("CAP_PROP_CONTRAST : '{}'".format(self.video_stream.get(cv2.CAP_PROP_CONTRAST)))
+        print("CAP_PROP_SATURATION : '{}'".format(self.video_stream.get(cv2.CAP_PROP_SATURATION)))
+        print("CAP_PROP_HUE : '{}'".format(self.video_stream.get(cv2.CAP_PROP_HUE)))
+        print("CAP_PROP_GAIN  : '{}'".format(self.video_stream.get(cv2.CAP_PROP_GAIN)))
+        print("CAP_PROP_CONVERT_RGB : '{}'".format(self.video_stream.get(cv2.CAP_PROP_CONVERT_RGB)))
+
